728729cd2b928bb7d601570466439bdc4049484e
harshchaurasiya2527 <126656417+harshchaurasiya2527@users.noreply.github.com>
2023-03-20 17:09:52 +0530

    Portfolio

---
+++ index.js
@@ -0,0 +1,12 @@
+<head>
+<script>
+function check()
+{
+    X=document.getElementByID("uname".value;)
+if(X=="")
+    alert("plesse enter name");
+else
+    alert("entered value is "+X);
+}
+</script>
+</head>
